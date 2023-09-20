6bb707ed2331fb1da10276b8a4016bb2be722ced
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-20 12:11:35 +0530

    this file consist of all operators and its usage

--- 02_operators.py
+++ 02_operators.py
@@ -1,3 +1,38 @@
-a = 3
-b = 4
-print("a +b = " , a+b)
+a=3
+b=4
+print("Addition of 3+4=",3+4)
+print("substractioon of 3-4=",3-4)
+print("multiplication of 3*4=",3*4)
+print("Division of 3/4=",3/4)
+#assignment operators
+a=32
+a+=2
+print(a)
+a-=2
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+#comparision operators
+b=(14<7)
+print(b)
+b=(14<=7)
+print(b)
+b=(14==7)
+print(b)
+b=(14>7)
+print(b)
+b=(14>=7)
+print(b)
+b=(14==7)
+print(b)
+b=(14!=7)
+print(b)
+
+# logical operators
+bool1=True
+bool2=False
+print("the value of bool1 and bool2 is ",(bool1 and bool2))
+print("the value of bool1 or bool2 is ",(bool1 or bool2))
+print("the value of bool1 not bool2 is ",(not bool2))
