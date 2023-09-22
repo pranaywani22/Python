2252e85a1101009a689cd3ca4d91662613a14eb4
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-22 10:04:52 +0530

    This file is structured in python to find the double spces in the string and replacing it with the single space

---
+++ finddoublespace.py
@@ -0,0 +1,7 @@
+'''
+Author:- Pranay Ajitkumar Wani
+to find double spaces in para and replace it with single spaces 
+'''
+st="This is a string with  double  spaces  ok  yes"
+st=st.replace("  "," ")
+print(st)
