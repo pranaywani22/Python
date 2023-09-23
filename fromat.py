c9db4c7210211c12e2ac7d53d4f96d9f81462d81
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-23 12:23:33 +0530

    This is a python program structured to add your name and date in a ready made format of a letter

---
+++ fromat.py
@@ -0,0 +1,7 @@
+'''
+Author:- Pranay Ajitkumar Wani
+'''
+letter="Hey Hi!, Pranay here and I am enjoying this python journey Thanks!"
+print(letter)
+formatted_letter = "Hey Hi!,\n\tPranay here and I am enjoying this python journey \nThanks!"
+print(formatted_letter)
