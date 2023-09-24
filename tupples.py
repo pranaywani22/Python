496a57f91e08acc6e7ca1bdf47a8b08f3ccd9e06
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-24 21:47:04 +0530

    Add files via upload

---
+++ tupples.py
@@ -0,0 +1,11 @@
+'''
+Author:- Pranay Ajitkumar Wani
+'''
+t=(1,3,4,5) #a tupple cant be edited onece created
+# print(t[0]) not possible
+# t([0])=90 cant be updated
+t1=() #an empty tupple
+t2=(7,) #a tupple with a single element
+print(t1)
+print(t2)
+print(t)
