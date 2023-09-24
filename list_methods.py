38b8ea7fe081c75e985dd70cb48497261934ddca
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-24 12:28:45 +0530

    Add files via upload

---
+++ list_methods.py
@@ -0,0 +1,20 @@
+'''
+Author:-Pranay Ajitkumar Wani
+'''
+#list methods
+l1=[1,2,5,4,8,45,76,23,9,65]
+print(l1)
+l1.sort() #this updates the list in order
+print(l1)
+l1.reverse() #this reverse the list
+print(l1)
+l1.append(10) #this add an element 10 in the end
+print(l1)
+l1.sort()
+print(l1)
+l1.insert(0,560)    #this inserts 560 in 0th index
+print(l1)  
+l1.pop(5)   #this deletes the number at index 5
+print(l1)
+l1.remove(23)   #this deletes 23 from the list
+print(l1)
---
+++ list_slicing.py
@@ -0,0 +1,5 @@
+#list slicing
+friends=['pranay','shaunak','nishant','shlok','milap']
+print(friends)
+print(friends[0:4])
+print(friends[-4:])
