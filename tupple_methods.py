373501710d123939af556709f033ce9bc57c1224
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-24 21:47:33 +0530

    Add files via upload

---
+++ tupple_methods.py
@@ -0,0 +1,6 @@
+'''
+Author:- Pranay Ajitkumar Wani
+'''
+t=(1,3,6,5,5,778,34)
+print(t.count(5))# this is used to count how many times 5 is repeated in the list
+print(t.index(5))# this is used to define on which index is 5 present
