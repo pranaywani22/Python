3557798397bb77158cbdb8eb5507149573a8ebda
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-25 11:14:18 +0530

    Add files via upload

---
+++ friendlist.py
@@ -0,0 +1,15 @@
+'''
+Author:- Pranay Ajitkumar Wani
+'''
+t1 = input("Enter Friend Name 1: \t ")
+t2 = input("Enter Friend Name 2: \t ")
+t3 = input("Enter Friend Name 3: \t ")
+t4 = input("Enter Friend Name 4: \t ")
+t5 = input("Enter Friend Name 5: \t ")
+t6 = input("Enter Friend Name 6: \t ")
+t7 = input("Enter Friend Name 7: \t ")
+t8 = input("Enter Friend Name 8: \t ")
+t9 = input("Enter Friend Name 9: \t ")
+t10 = input("Enter Friend Name 10: \t ")
+friendlist=[t1,t2,t3,t4,t5,t6,t7]
+print(friendlist)
