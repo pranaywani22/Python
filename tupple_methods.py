60e618ff63460271ff494042615a5aac19d16954
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-25 11:16:02 +0530

    Update tupple_methods.py

--- tupple_methods.py
+++ tupple_methods.py
@@ -1,5 +1,6 @@
 '''
 Author:- Pranay Ajitkumar Wani
+This file was created to study tupple methods
 '''
 t=(1,3,6,5,5,778,34)
 print(t.count(5))# this is used to count how many times 5 is repeated in the list
