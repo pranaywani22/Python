74f4deef75d3778d1f99b101acf79a2804689573
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-25 11:18:59 +0530

    Add files via upload

---
+++ marks&sorting.py
@@ -0,0 +1,12 @@
+'''
+Author:-Pranay Ajitkuamr Wani
+'''
+s1= input("Enter your name followed by marks: ")
+s2= input("Enter your name followed by marks: ")
+s3= input("Enter your name followed by marks: ")
+s4= input("Enter your name followed by marks: ")
+s5= input("Enter your name followed by marks: ")
+s6= input("Enter your name followed by marks: ")
+marks=[s1,s2,s3,s4,s5,s6]
+marks.sort()
+print(marks)
