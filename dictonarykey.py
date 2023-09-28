f2c5925b47e7c281c89b6ce24f36c1fff879505c
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-28 15:11:23 +0530

    Add files via upload

---
+++ dictonarykey.py
@@ -0,0 +1,15 @@
+'''
+Author:- Pranay Ajitkuamar Wani
+'''
+mydict={
+    "padh":"study",
+    "bhag":"run",
+    "khel":"play",
+    "pankha":"fan",
+    "mez":"table",
+    "khurchi":"chair"
+}
+print("Options of ", mydict.keys())
+a=input("Enter the Hindi word\n ")
+print("the meaning of given word is: ",mydic.get[a])#.get will not trough error if the word entered by the user is not present in the user.
+ 
