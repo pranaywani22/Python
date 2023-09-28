44b5b1291ea5776f39fea1a9f234c37fb64b66d4
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-28 15:08:59 +0530

    Add files via upload

---
+++ addlist.py
@@ -0,0 +1,12 @@
+'''
+Author:- Pranay Ajitkumar Wani
+the code is structure to add the numbers enterred in the list
+'''
+n1=input ("Enter Number 1 :\t")
+n2=input ("Enter Number 2 :\t")
+n3=input ("Enter Number 3 :\t")
+n4=input ("Enter Number 4 :\t")
+list=[n1,n2,n3,n4]
+print(list)
+print(list[0]+list[1]+list[2]+list[3])
+print(sum(list))
